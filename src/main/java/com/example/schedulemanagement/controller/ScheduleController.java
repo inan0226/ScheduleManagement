2d5e16536962d5cfc3107b596ef77f9cec2016a3
rladl <rladlsdks12@gmail.com>
2026-04-13 22:02:07 +0900

    레벨 2 완료

--- src/main/java/com/example/schedulemanagement/controller/ScheduleController.java
+++ src/main/java/com/example/schedulemanagement/controller/ScheduleController.java
@@ -3,14 +3,13 @@ package com.example.schedulemanagement.controller;
 import com.example.schedulemanagement.dto.CreateScheduleRequest;
 import com.example.schedulemanagement.dto.CreateScheduleResponse;
 import com.example.schedulemanagement.dto.GetScheduleResponse;
-import com.example.schedulemanagement.repository.ScheduleRepository;
 import com.example.schedulemanagement.sevice.ScheduleSevice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.CacheResponse;
+
 import java.util.List;
 
 @RestController
